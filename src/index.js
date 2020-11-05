921a449b7b9df3a09c1020070d005b22a80dd20a
Nazareno <naz.quiroga@gmail.com>
2020-11-05 16:52:48 -0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// From scratch, initialize the React app
+// Render an <App /> component
+// Create the <App /> component from scratch
+// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
+// like you're making a todo list with some hard-coded items on it
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
